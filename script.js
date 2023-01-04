65144b63447db6b881e999e448141bd4391dad0d
Musa Sayar <musasayar67@gmail.com>
2023-01-04 10:55:29 +0300

    Notification
    Notification

--- script.js
+++ script.js
@@ -1,12 +1,29 @@
 const count = document.querySelector(".notice");
 const countMessage = document.querySelectorAll(".message-color")
-
+const icon = document.querySelectorAll(".fa-solid")
 
 $(document).ready(function(){
     $(".read").click(function(){
         $("#Mark, #Angela, #Jacop").toggleClass("message-color")
         const  newMessage = document.querySelectorAll(".message-color")
         count.innerText=newMessage.length
+        $(".fa-solid").toggle()
+
+       
+
+    })
+})
+$(document).ready(function(){
+    $("#Mark").click(function(){
+        $(".mark").toggle()
+    })
+
+    $("#Angela").click(function(){
+        $(".angela").toggle()
+    })
+
+    $("#Jacop").click(function(){
+        $(".jacop").toggle()
     })
 })
 
@@ -14,9 +31,14 @@ $(document).ready(function(){
 
 countMessage.forEach((message)=>{
     message.addEventListener("click", () =>{
+        
         message.classList.toggle("message-color")
         const  newMessage = document.querySelectorAll(".message-color")
         count.innerText=newMessage.length
+        icon.classList.toggleClass()
+
+
+        
     }
     
     )
